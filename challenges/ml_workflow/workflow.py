b7764bd94e7e3880582ce518e58e88346d559fad
Kristian Rother <krother@academis.eu>
2024-05-16 21:07:06 +0200

    add workflow challenge

---
+++ challenges/ml_workflow/workflow.py
@@ -0,0 +1,25 @@
+"""
+In which order would you perform the following steps? Assemble them into a linear workflow.
+"""
+
+# deploy the model on a production machine
+
+# exploratory analysis (descriptive statistics and plots)
+
+# feature engineering (clean, transform and rearrange data)
+
+# get data
+
+# measure performance using the test set
+
+# measure performance using the training set
+
+# observe the model in production
+
+# optimize the model
+
+# set a business goal
+
+# split the data into a training and test set
+
+# train a model
